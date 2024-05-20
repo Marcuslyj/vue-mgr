2fd859f1163e9062e0468956ab6ade9a4f5339f2
liyongjie <liyongjie@twyfordtile.com>
2024-05-20 13:47:11 +0800

    feat: 支持指定从什么语言翻译成其他语言

--- src/lang/_readme.md
+++ src/lang/_readme.md
@@ -4,6 +4,9 @@
 
 远程翻译modules里边内容，根据zh.js生成{en|fr}.js
 
+指定from语言
+lang:trans -- from zh
+
 ## lang:output
 
 将modules里边的内容，组合生成labg/{zh|en|fr}.js
--- src/lang/scripts/translate.js
+++ src/lang/scripts/translate.js
@@ -16,8 +16,14 @@ import { remoteTranslate } from './utils.js'
 const { get } = lodash
 
 const langs = { "zh": "zh", "en": "en", "fr": "fr" }
+const remoteLangDict = {
+  [langs.zh]: "zh-Hans",
+  [langs.en]: "en",
+  [langs.fr]: "fr"
+}
 const argv = minimist(process.argv.slice(2));
 const [targetFolder] = argv._; // 指定需要翻译的子模块
+const { from = "zh" } = argv
 const root = process.cwd()
 const moduleRoot = 'src/lang/modules'
 const moduleRootPath = path.join(root, moduleRoot)
@@ -31,10 +37,10 @@ const hasLangJs = (lang, files) => files.includes(`${lang}.js`)
  */
 const transLang = async (lang, folderPath) => {
   const files = fg.sync([`${folderPath}/*.js`]).map(path => path.split('/').pop())
-  if (!hasLangJs(langs.zh, files)) return
+  if (!hasLangJs(langs[from], files)) return
 
   const moduleName = folderPath.split('/').pop()
-  const zh = (await import(`../modules/${moduleName}/zh.js`)).default // 读取中文翻译
+  const fromData = (await import(`../modules/${moduleName}/${from}.js`)).default // 读取中文翻译
 
   let result = {} // 结果对象
   const hasTargetLangFile = hasLangJs(lang, files)
@@ -45,17 +51,17 @@ const transLang = async (lang, folderPath) => {
 
   const fieldsToTrans = []
   const allFields = []
-  for (const [k, txt = ''] of Object.entries(zh)) {
+  for (const [k, txt = ''] of Object.entries(fromData)) {
     result[k] = Reflect.has(existTargetLang, k) ? existTargetLang[k] : ''
     // 空值才需要翻译，否则沿用
     if ((!get(result, k, '').trim()) && txt.trim()) fieldsToTrans.push(k)
     allFields.push(k)
   }
   // 创建翻译请求参数
-  const reqData = fieldsToTrans.map(field => ({ text: zh[field] }))
+  const reqData = fieldsToTrans.map(field => ({ text: fromData[field] }))
 
   if (reqData.length) {
-    const translations = (await remoteTranslate(reqData, lang)).data || []
+    const translations = (await remoteTranslate(reqData, lang, remoteLangDict[from])).data || []
     translations.forEach((t, i) => {
       result[fieldsToTrans[i]] = get(t, "translations[0].text", '')
     })
@@ -79,8 +85,9 @@ const excutor = () => {
     ? [`${moduleRoot}/${targetFolder}`] // 有指定目标模块
     : fg.sync([`${moduleRoot}/*`], { onlyDirectories: true, ignore: `${moduleRoot}/.*` }) // 所有模块
 
+  const targetLangs = Object.values(langs).filter(l => l !== from)
   folderPaths.forEach(folderPath =>
-    ['en', 'fr'].forEach(lang => transLang(lang, folderPath))
+    targetLangs.forEach(lang => transLang(lang, folderPath))
   )
 }
 
